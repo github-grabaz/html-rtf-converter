c35ab73904f483864e34ad1a8a0138eeaab34733
github-grabaz <davidrogerhill@gmail.com>
2020-11-15 00:07:09 -0700

    Add files via upload

---
+++ src/Node/ElementNode/H2ElementNode.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace HtmlToRtf\Node\ElementNode;
+
+use HtmlToRtf\Node\ElementNode;
+
+/**
+ * Class PElementNode
+ * @package HtmlToRtf\Node\ElementNode
+ */
+class H2ElementNode extends ElementNode
+{
+    /**
+     * parse node and create RTF string
+     * @return string
+     */
+    public function parse()
+    {
+        $prepend = '{\pard\sa200\sl276\slmult1';
+        $prepend .= '\f0\cf1\fs' . 30 . ' ';
+        $append = '\par}';
+
+        $css = $this->getAttribute('style');
+        $css = strtolower($css);
+
+        $styles = preg_split('/;/', $css);
+        $styles = array_map('trim', $styles);
+        foreach ($styles as $styleDef) {
+            if (empty($styleDef)) {
+                continue;
+            }
+
+            $style = preg_split('/:/', $styleDef);
+            $style = array_map('trim', $style);
+            switch ($style[0]) {
+                case 'text-align':
+                    switch ($style[1]) {
+                        case 'left':
+                            $prepend = $prepend . '\ql';
+                            break;
+
+                        case 'right':
+                            $prepend = $prepend . '\qr';
+                            break;
+
+                        case 'justify':
+                            $prepend = $prepend . '\qj';
+                            break;
+
+                        case 'center':
+                            $prepend = $prepend . '\qc';
+                            break;
+                    }
+                    break;
+            }
+        }
+        $this->setRtfPrepend($prepend . ' ');
+        $this->setRtfAppend($append);
+
+        return parent::parse();
+    }
+}
